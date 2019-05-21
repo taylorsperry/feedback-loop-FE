5d27bc5fc8fcde539a466d739eb5b9d3f8dfa9f5
“Kimberly <kimberly.achkio@gmail.com>
2019-05-21 08:06:50 -0600

    Experiment with inputs

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -7,24 +7,52 @@ export class Question extends Component {
     super();
     this.state = {
       questionTitle: '',
-      options: [
-        {value: 1}, 
-        {value: 2}, 
-        {value: 3}, 
-        {value: 4}, 
-        {value: 5}
-      ]
+      questionText: '',
+      // option_1: {pointValue: 1, questionText: ''},
+      // option_2: {pointValue: 1, questionText: ''},
+      // option_3: {pointValue: 1, questionText: ''},
+      // option_4: {pointValue: 1, questionText: ''},
+      // option_5: {pointValue: 1, questionText: ''}
+
+      // options: [
+      //   {pointValue: 1, questionText: ''},
+      //   {pointValue: 2, questionText: ''},
+      //   {pointValue: 3, questionText: ''},
+      //   {pointValue: 4, questionText: ''},
+      //   {pointValue: 5, questionText: ''}
+      // ]
     }
   }
+
+  handleChange = (e) => {
+    console.log(e.target)
+    const { name, value } = e.target
+    // this.state.options.forEach(option => {
+      // console.log(option)
+      this.setState({  questionText : value })
+    // })
+  }
+
+
   
 
 
   render() {
-    const options = this.state.options.map(option => {
+    const options = this.state.options.map((option, index) => {
       return <div key={shortid()}>
-                <input type="radio" name="radio" />
-                <input type="text" />
+                <input
+                  type="radio"
+                  name="radio" />
+                <input
+                  type="text"
+                  name={index}
+                  value={this.state.questionText}
+                  //this might be wrong
+                  onChange={this.handleChange} />
               </div>
+              //make 5 separate inputs in the return
+              //each will have its own radio and text input
+              //value on each one will correlate with what is in the state
     })
     return (
       <form>
