81ed07f56f092082a909bd652610e25be2659c84
“Kimberly <kimberly.achkio@gmail.com>
2019-05-21 19:30:20 -0600

    Add cohorts as props to populate dropdown

--- src/containers/RecipientForm/RecipientForm.js
+++ src/containers/RecipientForm/RecipientForm.js
@@ -1,34 +1,51 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 export class RecipientForm extends Component {
   constructor() {
     super()
     this.state = {
-      program: ['frontend', 'backend'],
-      cohorts: ['1811', '1810', '1808']
+      cohort_id: 0
     }
   }
 
-  //CDM will fetch cohorts
+  handleChange = (e) => {
+    const cohortId = parseInt(e.target.value)
+    this.setState({
+      cohort_id: cohortId
+    })
+  }
 
   render() {
+    const cohortList = this.props.cohorts.map(cohort => {
+      return <option key={cohort.id} value={cohort.id} >{cohort.name}</option>
+    })
     return(
       <div>
-        <h1>Select Recipients</h1>
-        Program
+        <h2>Select Recipients</h2>
+        <h3>Program</h3>
         <select>
-          <option>test choice 1</option>
-          <option>test choice 2</option>
+          <option>BE</option>
+          <option>FE</option>
         </select>
-        Cohort
-        <select>
-          <option>cohort 1</option>
-          <option>cohort 2</option>
+        <h3>Cohort</h3>
+        <select onChange={this.handleChange} >
+          <option value="0">select a cohort</option>
+          {cohortList}
         </select>
-        <button>Send</button>
+        <button disabled={!this.state.cohort_id}>Send</button>
       </div>
     )
   }
 }
 
-export default RecipientForm
+RecipientForm.propTypes = {
+  cohorts: PropTypes.array
+}
+
+export const mapStateToProps = (state) => ({
+  cohorts: state.cohorts
+})
+
+export default connect(mapStateToProps)(RecipientForm)
