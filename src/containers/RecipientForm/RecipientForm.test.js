59981427d304b9c988d65aa83eb4e9af90d77507
“Kimberly <kimberly.achkio@gmail.com>
2019-05-22 15:28:49 -0600

    Update imports

--- src/containers/RecipientForm/RecipientForm.test.js
+++ src/containers/RecipientForm/RecipientForm.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { RecipientForm, mapStateToProps } from './RecipientForm'
+import turingLogo from '../../assets/turingLogo.png'
+import Loader from '../../components/Loader/Loader'
 
 describe('RecipientForm', () => {
   let mockState
