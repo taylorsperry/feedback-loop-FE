2164b2a4ff1260304ee4d730bb3a3aaab09545a2
“Kimberly <kimberly.achkio@gmail.com>
2019-05-23 13:13:01 -0600

    Update snapshots and write mDTP test for Login.js

--- src/containers/Login/Login.test.js
+++ src/containers/Login/Login.test.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Login from './Login'
+import { Login, mapDispatchToProps } from './Login'
+import { setUser } from '../../actions'
 
 describe('Login', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(
-      <Login />
+  let mockSetUser
+  let wrapper
+
+  beforeEach(() => {
+    mockSetUser = jest.fn()
+    wrapper = shallow(
+      <Login 
+        setUser={mockSetUser}
+      />
     )
+  })
+
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should set state with a user when handleLogin is called', () => {
+    
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should return setUser to dispatch', () => {
+      const user = {
+        cohort: '1811',
+        id: 3,
+        name: 'Jessica Hansen',
+        program: 'F',
+        status: 'active'
+      }
+      const mockDispatch = jest.fn()
+      const actionToDispatch = setUser(user)
+      const mappedProps = mapDispatchToProps(mockDispatch)
+
+      mappedProps.setUser(user)
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    })
   })
 })
