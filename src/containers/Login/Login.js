efc767c5ed62ddd9e2c3247aaa41ed4f16fa00c7
“Kimberly <kimberly.achkio@gmail.com>
2019-05-23 11:25:37 -0600

    Set mock user in redux store on button click for Login.js

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { setUser } from '../../actions'
 
 export class Login extends Component {
+  constructor() {
+    super()
+    this.state = {
+        cohort: '1811',
+        id: 3,
+        name: 'Jessica Hansen',
+        program: 'F',
+        status: 'active'
+    }
+  }
+
+  handleLogin = () => {
+    this.props.setUser(this.state)
+  }
+
   render() {
     return(
       <div className='login-container'>
         <NavLink to='/dashboard'>
-          <button className='login-button'>
+          <button className='login-button' onClick={this.handleLogin}>
           Login
           </button>
         </NavLink>
@@ -15,4 +32,8 @@ export class Login extends Component {
   }
 }
 
-export default Login
+export const mapDispatchToProps = (dispatch) => ({
+  setUser: (user) => dispatch(setUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(Login)
