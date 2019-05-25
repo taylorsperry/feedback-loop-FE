acec59e60ab6da0d02f5ac2596e3aec9e70681a4
April Dagonese <adagonese@gmail.com>
2019-05-25 15:32:31 -0600

    Adds Turing logo with link to Turing.io

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Logo from '../../assets/turinglogo.png'
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return(
     <div className='header-container'>
-      <h2 className='feedback-loop-title-text'>Feedback Loop</h2>
+      <a href='https://turing.io/'><img className='turing-logo' src={Logo} alt="logo"/></a>
+      <Link to='/' className='feedback-loop-title'>
+        <h2 className='feedback-loop-title-text'>Feedback Loop</h2>
+      </Link>
     </div>
   )
 }
