add6f3487e98f2acafa99a4a16bcea4579afa703
“Kimberly <kimberly.achkio@gmail.com>
2019-05-26 16:58:33 -0600

    Resolve unit test conflicts

--- src/containers/RecipientForm/RecipientForm.test.js
+++ src/containers/RecipientForm/RecipientForm.test.js
@@ -49,6 +49,19 @@ describe('RecipientForm', () => {
     })
   })
 
+  it('should set state with the program', () => {
+    mockEvent = { target: { value: 'f'} }
+
+    wrapper.instance().handleProgram(mockEvent)
+
+    expect(wrapper.state()).toEqual({
+      cohort_id: 0,
+      program: 'f',
+      draggedStudent: {},
+      group: []
+    })
+  })
+
   it('should set state with the cohort id', () => {
     mockEvent = { target: { value: "19"} }
 
@@ -63,6 +76,36 @@ describe('RecipientForm', () => {
     })
   })
 
+  it('should set state with a student when onDrag is invoked', () => {
+    const mockEvent = { preventDefault: jest.fn() }
+    const mockStudent = { id: 99, name: 'April' }
+
+    wrapper.instance().onDrag(mockEvent, mockStudent)
+
+    expect(wrapper.state('draggedStudent')).toEqual({
+      id: 99,
+      name: "April"
+    })
+  })
+
+  it('should prevent default when student is dragged when onDragOver is invoked', () => {
+    const mockEvent = { preventDefault: jest.fn() }
+    wrapper.instance().onDragOver(mockEvent)
+    expect(mockEvent.preventDefault).toHaveBeenCalled()
+  })
+
+  it.skip('should update group, reset draggedStudent and call setCurrentCohort with leftover students when onDrop is invoked', () => {
+    const mockEvent = { preventDefault: jest.fn() }
+    const mockState = {
+      group: [],
+      draggedStudent: { id: 99, name: 'April'}
+    }
+    wrapper.setState({
+      group: mockState.group,
+      draggedStudent: mockState.draggedStudent
+    })
+  })
+  
   describe('mapStateToProps', () => {
     it('should return the expected state as props', () => {
       mockState = {
@@ -88,8 +131,6 @@ describe('RecipientForm', () => {
       expect(mappedProps).toEqual(expected)
     })
   })
-
-
 
   describe('mapDispatchToProps', () => {
     it('should return handlePost to dispatch', () => {
