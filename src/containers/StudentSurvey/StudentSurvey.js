81ddb7b4a69464d0aa1f573922721681f96815b9
Kim Myers <43019784+kimmichurri@users.noreply.github.com>
2019-05-27 10:44:28 -0600

    Update StudentSurvey.js

--- src/containers/StudentSurvey/StudentSurvey.js
+++ src/containers/StudentSurvey/StudentSurvey.js
@@ -8,7 +8,6 @@ export class StudentSurvey extends Component {
     super(props);
     this.state = {
       allResponses: [],
-      // fakeSurveys: this.props.studentSurveys
     }
   }
 
@@ -26,21 +25,11 @@ export class StudentSurvey extends Component {
       questions: foundSurvey.questions,
       members: foundSurvey.groups[0].members,
     })
-
-
-    // this.setState({
-    //   currStudent: {name: 'taylor', id: 18},
-    //   surveyName: this.props.studentSurveys[0].surveyName,
-    //   id: this.props.studentSurveys[0].id,
-    //   questions: this.props.studentSurveys[0].questions,
-    //   members: this.props.studentSurveys[0].groups[0].members
-    // })
   }
 
   
   renderResponse = () => {
     const { members, questions } = this.state
-    //There's no currStudent in state, took it out of linke 50 and didn't pass down on 53
     return members.map(member => {
       return <Response key={member.id} member={member} questions={questions} collectResponses={this.collectResponses}/>
     })
