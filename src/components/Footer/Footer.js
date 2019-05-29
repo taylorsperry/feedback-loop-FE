109567a79aa31c17453b435e9d0f90eda732ac72
“Kimberly <kimberly.achkio@gmail.com>
2019-05-29 16:22:38 -0600

    Turn Footer into component instead of container

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    }
-  }
-
-  render() {
+export const Footer = () => {
     return(
       <div className='footer'>
         Turing School of Software and Design, a Colorado Non-Profit Organization.
         © Copyright 2019 All Rights Reserved.
       </div>
     )
-  }
+  
 }
+
+export default Footer
